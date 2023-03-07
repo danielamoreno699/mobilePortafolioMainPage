8e4ec7f08caaa1a2c9d18b7294b686ceead36082
Daniela Moreno <danielamoreno699@gmail.com>
2023-03-07 12:16:10 -0500

    Add popup js

--- main.js
+++ main.js
@@ -96,3 +96,66 @@ const projectDetailList = [
     demoLink: '#',
   },
 ];
+
+// function of modla pop up window
+const onProjectButtonClick = (idProject) => {
+
+  let detail = projectDetailList.find((detail) => detail.id === idProject )
+  const modal_template_mobile = document.getElementById('modal-template-mobile')
+
+  let arrayString = []
+    
+  for(let el = 0; el < detail.technologies.length; el ++ ) {
+    let y =  `<li class="modal-btn-li">${detail.technologies[el]}</li>`
+    arrayString.push(y)
+  }
+
+  const projectString = arrayString.join('');
+
+  const detailHtml = `
+    <div class="modal">
+    <span>&times;</span>
+    <div class="modal-header"  id="modal-heading-${detail.id}" >
+        <img "src="${detail.imageLink}" alt="">
+    </div>
+
+    <div class="modal-body">
+        <h3 class="modal-title"> ${detail.name} </h3>
+            <div class="modal-text">    
+                <p class="modal-text-element one"> ${detail.company}  </p>
+                 <!--- <img class="counter-png" src="./assets/Counter.png" alt=""> -->
+                <p class="modal-text-element two"> ${detail.role}</p>
+                 <!--- <img class="counter-png" src="./assets/Counter.png" alt=""> -->
+                <p class="modal-text-element"> ${detail.year}</p>
+            </div>
+        <p class="modal-description"> ${detail.description}  </p>          
+        
+            <div class="modal-container-btns">
+                <div class="modal-buttons-are">
+                <ul class="modal-button-group">
+                    ${projectString}
+                             
+                </ul> 
+            </div>
+            <div class="modal-box-buttons-group">
+                <div class="modal-box-button">
+                    <button class="btn-modal">
+                      See live
+                    </button>
+                </div>
+                <div class="modal-box-button">
+                    <button class="btn-modal">
+                      See Source
+                    </button>
+                </div>
+            </div>
+           
+             </div>
+        </div>
+    </div>
+</div>`;
+
+  // eslint-disable-next-line camelcase
+  modal_template_mobile.innerHTML = detail_html;
+  modal_template_mobile.style.display = 'block';
+};
