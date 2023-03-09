29900b89041e6a810542ac744e4803f416403fb3
Daniela Moreno <danielamoreno699@gmail.com>
2023-03-08 23:57:56 -0500

    Added more functionalities to js

--- main.js
+++ main.js
@@ -133,24 +133,70 @@ const form = document.getElementById('form');
 form.addEventListener('submit', validateInputEmail);
 
 // function to display Modal depending on media query size
-const onMediaQueryModal = () => {
-  const modalTemplateMobile = document.getElementById('modal-template-mobile');
-  const modalTemplateWeb = document.getElementById('modal-template-web');
-
+const onMediaQueryModal = (width) => {
   if (window.matchMedia('(min-width: 767px)').matches) {
     console.log('matches');
-    modalTemplateWeb.style.visibility = ('visible');
-  } else {
-    console.log('no matches');
-    modalTemplateMobile.style.visibility = ('visible');
+    return true;
   }
+  console.log('no matches');
+  return false;
+};
+
+// function of pop up modal mobile
+
+const effectsOnMobile = () => {
+  const modalTemplateMobile = document.getElementById('modal-template-mobile');
+
+  const arBtns = document.getElementsByClassName('btn-card');
+
+  Array.from(arBtns).forEach((el) => {
+    el.addEventListener('click', () => {
+      modalTemplateMobile.style.display = 'block';
+
+      document.getElementById('main-page-text').style.filter = 'blur(5px)';
+      const toolbar = document.getElementById('toolbar');
+      toolbar.style.backgroundColor = '#C1C7D0';
+      toolbar.style.opacity = '0.7';
+
+      const navbar = document.getElementById('navbar');
+      navbar.style.backgroundColor = '#C1C7D0';
+      navbar.style.opacity = '0.7';
+
+      const mainPage = document.getElementById('main-page');
+      mainPage.style.backgroundColor = '#C1C7D0';
+
+      modalTemplateMobile.style.left = ('13%');
+      modalTemplateMobile.style.top = ('-88%');
+      modalTemplateMobile.style.position = ('relative');
+      modalTemplateMobile.style.zIndex = '1';
+      modalTemplateMobile.style.opacity = ('1');
+    });
+  });
+};
+
+// function to close mobile modal window
+
+const MobileModalClose = () => {
+  const modalTemplateMobile = document.getElementById('modal-template-mobile');
+  modalTemplateMobile.style.display = ('none');
+
+  document.getElementById('main-page-text').style.filter = 'blur(0px)';
+  const toolbar = document.getElementById('toolbar');
+  toolbar.style.backgroundColor = 'white';
+  toolbar.style.opacity = '1';
+
+  const navbar = document.getElementById('navbar');
+  navbar.style.backgroundColor = 'white';
+  navbar.style.opacity = '1';
+
+  const mainPage = document.getElementById('main-page');
+  mainPage.style.backgroundColor = 'white';
 };
 
 // function to pop up modal web
 
 const effectsOnModalWeb = () => {
   const modalTemplateWeb = document.getElementById('modal-template-web');
-
   const arBtns = document.getElementsByClassName('btn-card');
 
   Array.from(arBtns).forEach((el) => {
@@ -169,6 +215,7 @@ const effectsOnModalWeb = () => {
       const mainPage = document.getElementById('main-page');
       mainPage.style.backgroundColor = '#C1C7D0';
       mainPage.style.opacity = '0.7';
+      mainPage.style.height = '100vh';
       console.log('click');
 
       modalTemplateWeb.style.left = ('-27%');
@@ -194,20 +241,22 @@ const closeModalWeb = () => {
   const mainPage = document.getElementById('main-page');
   mainPage.style.backgroundColor = 'white';
   mainPage.style.opacity = '1';
-  mainPage.style.height = '26%';
   document.getElementById('main-page-text').style.filter = 'blur(0px)';
   console.log('click close');
 };
 
-// function of modla pop up window
-const onProjectButtonClick = (idProject) => {
-  onMediaQueryModal();
-  effectsOnModalWeb();
-
-  const detail = projectDetailList.find((detail) => detail.id === idProject);
-
+// function of modal pop up window
+const onProjectButtonClick = (idProject, event) => {
   const modalTemplateMobile = document.getElementById('modal-template-mobile');
   const modalTemplateWeb = document.getElementById('modal-template-web');
+
+  if ((onMediaQueryModal(event.screenX)) === true) {
+    effectsOnModalWeb();
+  } else {
+    effectsOnMobile();
+  }
+
+  const detail = projectDetailList.find((detail) => detail.id === idProject);
 
   const arrayString = [];
   for (let el = 0; el < detail.technologies.length; el++) {
@@ -218,6 +267,39 @@ const onProjectButtonClick = (idProject) => {
 
   // dynamic template for mobile
   const detailHtmlMobile = `
+  <div class="modal-header"  id="modal-${detail.id}" >
+  <span onclick="MobileModalClose()" >&times;</span>
+      <img class="img-modal" src= ${detail.imageLink2} alt="">
+  </div>
+  <div class="modal-body">
+      <h3 class="modal-title"> ${detail.name} </h3>
+          <div class="modal-text">    
+              <p class="modal-text-element one"> ${detail.company} </p>
+              <p class="modal-text-element two"> ${detail.role}</p>
+              <p class="modal-text-element"> ${detail.year}</p>
+          </div>  
+           <p class="modal-description">  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>          
+           <div class="modal-container-btns">
+              <ul class="modal-button-group">
+                   ${projectString}      
+              </ul> 
+          </div>
+          <div class="modal-box-buttons-group" >
+              <div class="modal-box-button">
+                  <button class="btn-modal">
+                    See live
+                    <img class="img-btn src"src="./assets/githubblue.png" alt="">
+                  </button>
+              </div>
+              <div class="modal-box-button">
+                  <button class="btn-modal">
+                    See source
+                    <img class="img-btn src"src="./assets/githubblue.png" alt="">
+                  </button>
+              </div>
+            
+          </div>
+  </div>
   
    `;
 
