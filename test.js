c363d975e3b5fa6eaeecae26eba1e1c9b328b948
Daniela Moreno <danielamoreno699@gmail.com>
2023-03-10 00:31:22 -0500

    Fix errors

--- test.js
+++
@@ -1,9 +0,0 @@
-const Objects = {
-    name: 'luis',
-    edad: 18,
-  };
-
-for (let i = 0; i < Objects.length; i ++) {
- const inputValue = Objects[i];
-    console.log('parsed', Objects[inputValue]);
- }
