2b26016f06068a73ea6cca2a32e89a4be939366c
Daniela Moreno <danielamoreno699@gmail.com>
2023-03-10 18:50:59 -0500

    Update files

--- main.js
+++ main.js
@@ -148,6 +148,8 @@ const onMediaQueryModalMobile = (width) => {
 };
 
 const effectsOnMobile = () => {
+  const backgroundModalmobile = document.getElementById('modal-wraper-mobile');
+
   const modalTemplateMobile = document.getElementById('modal-template-mobile');
 
   const arBtns = document.getElementsByClassName('btn-card');
@@ -155,17 +157,49 @@ const effectsOnMobile = () => {
   Array.from(arBtns).forEach((el) => {
     el.addEventListener('click', () => {
       modalTemplateMobile.style.display = 'block';
+      backgroundModalmobile.style.display = 'block';
+      backgroundModalmobile.style.backdropFilter = 'blur(5px)';
+      backgroundModalmobile.style.position = ('fixed');
+      backgroundModalmobile.style.zIndex = ('1');
+      backgroundModalmobile.style.top = ('0');
+      backgroundModalmobile.style.width = ('100%');
+      /*backgroundModalmobile.style.height = ('100%');*/
+      backgroundModalmobile.style.overflow = ('auto');
+      backgroundModalmobile.style.background = ('rgba(0,0,0,0.2)');
     });
   });
 };
 
 const MobileModalClose = () => {
+  const backgroundModalmobile = document.getElementById('modal-wraper-mobile');
   const modalTemplateMobile = document.getElementById('modal-template-mobile');
+
   modalTemplateMobile.style.display = ('none');
+  backgroundModalmobile.style.display = ('none');
+  backgroundModalmobile.style.backdropFilter = '';
+  backgroundModalmobile.style.position = '';
+  backgroundModalmobile.style.zIndex = '';
+  backgroundModalmobile.style.top = '';
+  backgroundModalmobile.style.width = '';
+  backgroundModalmobile.style.height = '';
+  backgroundModalmobile.style.overflow = '';
+  backgroundModalmobile.style.background = '';
+
 };
 
 const effectsOnModalWeb = () => {
+  const backgroundModalWeb = document.getElementById('modal-wraper-web');
+  backgroundModalWeb.style.display = 'block';
+  backgroundModalWeb.style.position = ('fixed');
+  backgroundModalWeb.style.zIndex = ('1');
+  backgroundModalWeb.style.top = ('0');
+  backgroundModalWeb.style.width = ('100%');
+  backgroundModalWeb.style.height = ('100%');
+  backgroundModalWeb.style.overflow = ('auto');
+  backgroundModalWeb.style.backdropFilter = ('blur(5px)');
+
   const modalTemplateWeb = document.getElementById('modal-template-web');
+  modalTemplateWeb.style.display = ('block');
   const arBtns = document.getElementsByClassName('btn-card');
 
   Array.from(arBtns).forEach((el) => {
@@ -176,10 +210,12 @@ const effectsOnModalWeb = () => {
 };
 
 const closeModalWeb = () => {
+  const backgroundModalWeb = document.getElementById('modal-wraper-web');
+  backgroundModalWeb.style.display = 'none';
+
   const modalTemplateWeb = document.getElementById('modal-template-web');
   modalTemplateWeb.style.display = 'none';
 };
-
 
 // function of modal pop up window
 const onProjectButtonClick = (idProject, event) => {
@@ -204,90 +240,96 @@ const onProjectButtonClick = (idProject, event) => {
 
   // dynamic template for mobile
   const detailHtmlMobile = `
-  <div class="modal-header"  id="modal-${detail.id}" >
-  <span onclick="MobileModalClose()" >&times;</span>
-      <img class="img-modal" src= ${detail.imageLink2} alt="">
+
+  <button class="close-btn-mobile"  onclick="MobileModalClose()">&times;</button>
+  <div class="modal-header"  id="modal-heading-${detail.id}" >
+      <img class="img-modal" src=${detail.imageLink2} alt="">
   </div>
-  <div class="modal-body">
-      <h3 class="modal-title"> ${detail.name} </h3>
-          <div class="modal-text">    
-              <p class="modal-text-element one"> ${detail.company} </p>
-              <p class="modal-text-element two"> ${detail.role}</p>
-              <p class="modal-text-element"> ${detail.year}</p>
-          </div>  
-           <p class="modal-description">  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>          
-           <div class="modal-container-btns">
-              <ul class="modal-button-group">
-                   ${projectString}      
-              </ul> 
-          </div>
-          <div class="modal-box-buttons-group" >
-              <div class="modal-box-button">
-                  <button class="btn-modal">
-                    See live
-                    <img class="img-btn src"src="./assets/githubblue.png" alt="">
-                  </button>
-              </div>
-              <div class="modal-box-button">
-                  <button class="btn-modal">
-                    See source
-                    <img class="img-btn src"src="./assets/githubblue.png" alt="">
-                  </button>
-              </div>
-            
-          </div>
-  </div>
+
+<div class="modal-content-mobile">
+  <h3 class="modal-title"> ${detail.name} </h3>
+  <div class="modal-text">    
+      <p class="modal-text-element one"> ${detail.company} </p>
+      <p class="modal-text-element two"> ${detail.role}</p>
+      <p class="modal-text-element"> ${detail.year}</p>
+  </div>  
+  <p class="modal-description">  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>          
   
-   `;
+  <div class="modal-container-btns">
+    
+     <ul class="modal-button-group">
+     ${projectString}  
+                  
+     </ul> 
+
+ </div>
+ <div class="modal-box-buttons-group" >
+  <div class="modal-box-button">
+      <button class="btn-modal">
+        See live
+        <img class="img-btn src"src="./assets/githubblue.png" alt="">
+      </button>
+  </div>
+  <div class="modal-box-button">
+      <button class="btn-modal">
+        See source
+        <img class="img-btn src"src="./assets/githubblue.png" alt="">
+      </button>
+  </div>
+
+</div>
+
+
+  </div>
+ 
+    `;
 
   modalTemplateMobile.innerHTML = detailHtmlMobile;
 
   // dynamic template for web
   const detailHtmlWebModal = `
-  <div class="modal-header-web " id="modal-heading-${detail.id}">
-  <div class="modal-text-content-web">
-      <h2 class="modal-title-web" > ${detail.name}</h2>
-      <span onclick="closeModalWeb()" >&times;</span>
+  <div class="modal-header-web" id="modal-${detail.id}">
+  <h2 class="modal-title-web" > ${detail.name} </h2>
+  <button class="close-btn-web"  onclick="closeModalWeb()" >&times;</button>
+</div>
+<div class="modal-content-web">
+  <div class="modal-text-web">    
+      <p class="modal-text-element-web one"> ${detail.company}</p>
+       
+      <p class="modal-text-element-web two">${detail.role}</p>
+       
+      <p class="modal-text-element-web">${detail.year}</p>
   </div>
-      <div class="modal-text-web">    
-          <p class="modal-text-element-web one"> ${detail.company} </p>
+  <img class="modal-img"src= ${detail.imageLink2} alt="" style="height: 586px; width: 1108px">
+</div>
+
+<div class="modal-body-web"> 
+  <p class="modal-description-web">
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea
+  </p>  
+  <div class="modal-container-btns">
+             
+      <ul class="button-group-web-modal">
+            ${projectString}                 
+      </ul> 
+      <div class="modal-box-buttons-group-web">
+          <div class="modal-box-button-web">
+               <button class="btn-modal-web">
+                 See live
+                 <img class="img-btn live"src="./assets/githubblue.png" alt="">
+               </button>
+           </div>
            
-          <p class="modal-text-element-web two">${detail.role}</p>
-           
-          <p class="modal-text-element-web">${detail.year}</p>
-      </div>
-  <img class="modal-img"src=${detail.imageLink2} alt="" style="height: 586px; width: 1108px">
-  </div>
-  <div class="modal-body-web"> 
-      <p class="modal-description-web">
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea
-      </p>          
-      <!--Here starts labels and button-->
-      <div class="modal-container-btns-web">
-              
-              <ul class="modal-button-group-web">
-                  ${projectString}
-              </ul> 
-      
-          <div class="modal-box-buttons-group-web">
-             <div class="modal-box-button-web">
-                  <button class="btn-modal-web">
-                    See live
-                    <img class="img-btn live"src="./assets/githubblue.png" alt="">
-                  </button>
-              </div>
-              
-              <div class="modal-box-button-web">
-                  <button class="btn-modal-web">
-                    See Source
-                    <img class="img-btn src"src="./assets/githubblue.png" alt="">
-                  </button>
-              </div>
-          </div>
-          
-      </div>
-  
-  </div>
+           <div class="modal-box-button-web">
+               <button class="btn-modal-web">
+                 See Source
+                 <img class="img-btn src"src="./assets/githubblue.png" alt="">
+               </button>
+           </div>
+       </div>
+ 
+  </div>     
+</div>
   `;
   modalTemplateWeb.innerHTML = detailHtmlWebModal;
 };
